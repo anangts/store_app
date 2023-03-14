f71b2ebe8f6242431fb383c6c3fa37c10972175d
anangtsap <anangtsap@gmail.com>
2023-03-14 20:39:23 +0700

    remove skip function after signout

--- lib/firebase/wrapper.dart
+++ lib/firebase/wrapper.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:store_app/firebase/auth.dart';
-import 'package:store_app/pages/category_page.dart'; 
+import 'package:store_app/pages/category_page.dart';
+import 'package:store_app/pages/login_page.dart';
+// ignore: unused_import
 import 'package:store_app/pages/splashscreen.dart';
 
 class Wrapper extends StatefulWidget {
@@ -13,15 +15,12 @@ class Wrapper extends StatefulWidget {
 }
 
 class _WrapperState extends State<Wrapper> {
-  //late SubCategory subCategory;
-  //final List<SubCategory> subcategories = Utils.getMockedCategories();
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: Auth().authStateChange,
       builder: (context, snapshot) {
-        return snapshot.hasData ? CategoryPage() : const SplashScreen();
+        return snapshot.hasData ? CategoryPage() : const LoginPage();
       },
     );
   }
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -14,53 +14,36 @@ class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
-      appBar: const MainAppBar(),
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Padding(
+        drawer: const Drawer(),
+        appBar: const MainAppBar(),
+        body: Stack(children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            const Padding(
                 padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
                 child: Text('Select Categories',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         fontFamily: 'Roboto',
                         fontSize: 24.0,
-                        color: AppColor.mainColor)),
-              ),
-              Expanded(
+                        color: AppColor.mainColor))),
+            Expanded(
                 child: ListView.builder(
-                  padding: const EdgeInsets.only(bottom: 60.0),
-                  itemCount: categories.length,
-                  itemBuilder: (BuildContext ctx, int index) {
-                    return CategoryCard(
-                      category: categories[index],
-                      onPress: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SubCategoryPage(
-                              selectedCategory: categories[index],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
+                    padding: const EdgeInsets.only(bottom: 60.0),
+                    itemCount: categories.length,
+                    itemBuilder: (BuildContext ctx, int index) {
+                      return CategoryCard(
+                          category: categories[index],
+                          onPress: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SubCategoryPage(
+                                        selectedCategory: categories[index])));
+                          });
+                    }))
+          ]),
           const Positioned(
-            bottom: 0.0,
-            right: 0.0,
-            left: 0.0,
-            child: CategoryButtonBar(),
-          ),
-        ],
-      ),
-    );
+              bottom: 0.0, right: 0.0, left: 0.0, child: CategoryButtonBar())
+        ]));
   }
 }
